05233d4763a002c2dd2a94376e58aa276b59e70a
badal-patil <80229011+badal-patil@users.noreply.github.com>
2024-07-25 00:42:24 +0100

    Rock-Paper-Scissors

--- basic codes/RPS.py
+++ basic codes/RPS.py
@@ -1,1 +1,43 @@
+# The Python Random module is a built-in feature of Python. 
+# At its core, a random function in Python generates pseudo-random numbers – these numbers may appear random, but they are generated using a deterministic algorithm.
+#  This module facilitates various random actions, including generating random numbers and selecting random values from lists or strings.
+
+import random
+option = ["Rock", "Paper", "Scissors"]
+computer = random.choice(option)
+# list of string provided from random
+player = False
+cpu_score = 0
+player_score = 0
+while True:
+    player = input("Rock, Paper or  Scissors?").capitalize()
+    ## Conditions of Rock,Paper and Scissors
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "smashes", computer)
+            player_score+=1
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "covers", computer)
+            player_score+=1
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "cut", computer)
+            player_score+=1
+    elif player=='End':
+        print("Final Scores:")
+        print(f"CPU:{cpu_score}")
+        print(f"Player:{player_score}")
+        break
 
